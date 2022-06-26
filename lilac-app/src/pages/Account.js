69a85970d024140501d307ddaf80bc53673b6b5c
Timothy G <timg51237@gmail.com>
2022-06-25 22:12:45 -0400

    account into own pgae

--- lilac-app/src/pages/Account.js
+++ lilac-app/src/pages/Account.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect, useContext } from 'react';
 import axios from 'axios';
 import { Button, Tabs, Input, Upload, message, Card, Col, Row } from 'antd';
-import Login from './Login.js';
 import WhiteBackground from '../components/WhiteBackground.js';
 import UserContext from '../UserContext';
 
@@ -9,13 +8,14 @@ function capitalize(str) {
     return str[0].toUpperCase() + str.substring(1);
 }
 
-function Account() {
+function Account({ address }) {
     const { web3 } = useContext(UserContext);
 
-    const walletAddr = web3[0].slice(0, 4) + '...' + web3[0].slice(-4, -1);
+    console.log('rendering account');
+
+    const walletAddr = address.slice(0, 6) + '...' + address.slice(-5, -1);
 
     const [name, setName] = useState('Test Name');
-    const [toLogin, setToLogin] = useState(false);
     const [reputationScore, setReputationScore] = useState(50);
     const [githubUsername, setGithubUsername] = useState('@TestUsername');
     const [events, setEvents] = useState([]);
@@ -24,10 +24,10 @@ function Account() {
         const fetchEvents = async () => {
             console.log('fetching data');
             const compound = await axios.get(`${process.env.REACT_APP_SERVER_URL}api/data/`, {
-                params: { address: web3[0], protocol: 'compound' },
+                params: { address, protocol: 'compound' },
             });
             const dydx = await axios.get(`${process.env.REACT_APP_SERVER_URL}api/data/`, {
-                params: { address: web3[0], protocol: 'dydx' },
+                params: { address, protocol: 'dydx' },
             });
 
             setEvents([...compound.data.documents, ...dydx.data.documents]);
@@ -35,13 +35,13 @@ function Account() {
 
         const fetchUser = async () => {
             await axios.post(`${process.env.REACT_APP_SERVER_URL}api/user/updateCompoundCredit`, {
-                address: web3[0],
+                address,
             });
 
             const { data } = await axios.get(
                 `${process.env.REACT_APP_SERVER_URL}api/user/getUserData`,
                 {
-                    params: { address: web3[0] },
+                    params: { address },
                 }
             );
 
@@ -52,136 +52,110 @@ function Account() {
 
         fetchEvents();
         fetchUser();
-    }, [web3]);
+    }, [address]);
 
     console.log(walletAddr);
 
-    function handleSubmit() {
-        console.log('beans');
-    }
+    const handleSubmit = async () => {
+        await axios.put(`${process.env.REACT_APP_SERVER_URL}api/user/saveUserData`, {
+            address,
+            github: githubUsername,
+            name,
+        });
+        console.log('updated');
+    };
 
     return (
-        <>
-            {' '}
-            {toLogin === true ? (
-                <Login />
-            ) : (
-                <div className="form">
-                    <div style={{ fontSize: '40px', fontWeight: '700' }}>My Profile</div>
-                    <div className="contents-align">
-                        <div className="form-display">
-                            <div
-                                style={{
-                                    position: 'absolute',
-                                    zIndex: '-1',
-                                    display: 'flex',
-                                    alignItems: 'center',
-                                    flexDirection: 'column',
-                                    width: '50vw',
-                                }}
-                            >
-                                <WhiteBackground />
-                            </div>
-                            <div
-                                style={{
-                                    zIndex: '1',
-                                    display: 'flex',
-                                    alignItems: 'center',
-                                    flexDirection: 'column',
-                                    height: '100%',
-                                    padding: '0 5vw 0 5vw',
-                                    marginTop: '20vh',
-                                    width: '30vw',
-                                }}
-                            >
-                                <div style={{ fontSize: '15px', marginTop: '2vh' }}>Name</div>
-                                <Input
-                                    style={{ borderRadius: '1vw', size: 'small' }}
-                                    onChange={(event) => setName(event.target.value)}
-                                    defaultValue={name}
-                                />
-
-                                <div style={{ fontSize: '15px', marginTop: '2vh' }}>
-                                    Wallet Address
-                                </div>
-                                <h3>{walletAddr}</h3>
-
-                                <div style={{ fontSize: '15px', marginTop: '2vh' }}>
-                                    GitHub Username
-                                </div>
-                                <Input
-                                    style={{ borderRadius: '1vw', size: 'small' }}
-                                    onChange={(event) => setGithubUsername(event.target.value)}
-                                    defaultValue={githubUsername}
-                                />
-
-                                <div style={{ fontSize: '15px', marginTop: '2vh' }}>
-                                    Reputation Score
-                                </div>
-                                <h2>{reputationScore}/1000</h2>
-
-                                <Button
-                                    variant="primary"
-                                    type="submit"
-                                    onClick={handleSubmit}
-                                    className="button button--secondary"
-                                >
-                                    SAVE
-                                </Button>
-                            </div>
-                        </div>
-                    </div>
-                    <div style={{ fontSize: '20px', marginBottom: '2vh', fontWeight: '700' }}>
-                        Activity
-                    </div>
-
+        <div className="form">
+            <div style={{ fontSize: '40px', fontWeight: '700' }}>My Profile</div>
+            <div className="contents-align">
+                <div className="form-display">
                     <div
-                        className="site-card-wrapper"
                         style={{
+                            position: 'absolute',
+                            zIndex: '-1',
                             display: 'flex',
-                            flexDirection: 'row',
-                            justifyContent: 'space-around',
-                            flexWrap: 'wrap',
-                            width: '70vw',
-                            backgroundColor: '#FFDDFF',
+                            alignItems: 'center',
+                            flexDirection: 'column',
+                            width: '50vw',
                         }}
                     >
-                        {events.map((event) => (
-                            <Card title={capitalize(event.type)} bordered={false}>
-                                <p>{capitalize(event.protocol)}</p>
-                                <p>Reputation Points: {event.magnitude}</p>
-                            </Card>
-                        ))}
+                        <WhiteBackground />
                     </div>
-                    <br />
+                    <div
+                        style={{
+                            zIndex: '1',
+                            display: 'flex',
+                            alignItems: 'center',
+                            flexDirection: 'column',
+                            height: '100%',
+                            padding: '0 5vw 0 5vw',
+                            marginTop: '20vh',
+                            width: '30vw',
+                        }}
+                    >
+                        <div style={{ fontSize: '15px', marginTop: '2vh' }}>Name</div>
+                        {address == web3[[0]] ? (
+                            <Input
+                                style={{ borderRadius: '1vw', size: 'small' }}
+                                onChange={(event) => setName(event.target.value)}
+                                value={name}
+                            />
+                        ) : (
+                            <h3>{name}</h3>
+                        )}
+
+                        <div style={{ fontSize: '15px', marginTop: '2vh' }}>Wallet Address</div>
+                        <h3>{walletAddr}</h3>
+
+                        <div style={{ fontSize: '15px', marginTop: '2vh' }}>GitHub Username</div>
+                        {address == web3[[0]] ? (
+                            <Input
+                                style={{ borderRadius: '1vw', size: 'small' }}
+                                onChange={(event) => setGithubUsername(event.target.value)}
+                                value={githubUsername}
+                            />
+                        ) : (
+                            <h3>{githubUsername}</h3>
+                        )}
+
+                        <div style={{ fontSize: '15px', marginTop: '2vh' }}>Reputation Score</div>
+                        <h2>{reputationScore}/1000</h2>
+
+                        <Button
+                            variant="primary"
+                            type="submit"
+                            onClick={handleSubmit}
+                            className="button button--secondary"
+                        >
+                            SAVE
+                        </Button>
+                    </div>
                 </div>
-            )}
-        </>
+            </div>
+            <div style={{ fontSize: '20px', marginBottom: '2vh', fontWeight: '700' }}>Activity</div>
+
+            <div
+                className="site-card-wrapper"
+                style={{
+                    display: 'flex',
+                    flexDirection: 'row',
+                    justifyContent: 'space-around',
+                    flexWrap: 'wrap',
+                    width: '70vw',
+                    // backgroundColor: '#FFDDFF',
+                }}
+            >
+                {events.map((event) => (
+                    <Card title={capitalize(event.type)} bordered={false}>
+                        <p>{capitalize(event.protocol)}</p>
+                        <p>Reputation Points: {event.magnitude}</p>
+                    </Card>
+                ))}
+            </div>
+            <br />
+        </div>
     );
 }
 
 export default Account;
-
-/**deed name
- * legal name 
- * user comments about the deed
- * coordinates in json format (look into mapping library)
- * [
-        [
-            1,
-            2
-        ],
-        [
-            1,
-            3
-        ],
-        [
-            2,
-            2
-        ],
-        [
-            2,
-            3
-        ]
-  JSON.stringify(your json)
- */
